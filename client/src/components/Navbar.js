fbbb65af64e26f5d701a3db6959bca86ff83cbe3
Kelsey Halley <kgb283@gmail.com>
2022-01-29 13:14:09 -0700

    spelling error

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = (props) => {
             <body>
                 <nav>
                     <img className="img" src="https://ih1.redbubble.net/image.378357496.4455/flat,750x,075,f-pad,750x1000,f8f8f8.u4.jpg" alt="piratecat"></img>
-                    <a className="a" href="/">Pawrites</a>
+                    <a className="a" href="/">PAWrates of the Kittenbean</a>
                     <a  className="a" href="/create">Add One</a>
                     <div className="animation start-home"></div>
                 </nav>
